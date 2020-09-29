5d044876f054decc57ec390fdf758166ac6f8fb1
Cinchoo <Cinchoofrx@gmail.com>
2020-09-29 16:19:48 -0400

    Fixes

--- ChoETL.NACHATest/Program.cs
+++ ChoETL.NACHATest/Program.cs
@@ -13,17 +13,69 @@ namespace ChoETL.NACHATest
         static void Main(string[] args)
         {
             ChoETLFrxBootstrap.TraceLevel = System.Diagnostics.TraceLevel.Verbose;
-            WriteACHFile();
+            ReadNWriteACHFile();
         }
 
-        static void ReadACHFile()
+        private static void ReadNWriteACHFile()
         {
-            foreach (var r in new ChoNACHAReader("20151027B0000327P018CHK.ACH"))
-            {
+            using (ChoNACHAReader r = new ChoNACHAReader("20151027B0000327P018CHK.ACH"))
                 Console.WriteLine(r.ToStringEx());
+
+            ChoNACHAConfiguration config = new ChoNACHAConfiguration();
+            config.DestinationBankRoutingNumber = "123456789";
+            config.OriginatingCompanyId = "123456789";
+            config.DestinationBankName = "PNC Bank";
+            config.OriginatingCompanyName = "Microsoft Inc.";
+            config.ReferenceCode = "Internal Use Only.";
+            config.BlockingFactor = 10;
+            using (var nachaWriter = new ChoNACHAWriter(@"C:\Projects\GitHub\ChoETL.NACHA\ChoETL.NACHATest\bin\Debug\NACHA_TEST.txt", config))
+            {
+                nachaWriter.Configuration.ErrorMode = ChoETL.ChoErrorMode.ThrowAndStop;
+
+                using (var bw1 = nachaWriter.CreateBatch(200))
+                {
+                    using (var entry1 = bw1.CreateDebitEntryDetail(20, "123456789", "1313131313", 22.505M, "ID Number", "ID Name", "Desc Data"))
+                    {
+                        entry1.CreateAddendaRecord("Monthly bill");
+                    }
+                    using (var entry2 = bw1.CreateCreditEntryDetail(20, "123456789", "1313131313", 22.505M, "ID Number", "ID Name", "Desc Data"))
+                    {
+
+                    }
+                }
+                using (var bw2 = nachaWriter.CreateBatch(200))
+                {
+                }
             }
         }
+        static void WriteACHFile1()
+        {
+            using (ChoNACHAReader fr = new ChoNACHAReader("20151027B0000327P018CHK.ACH"))
+            {
+                ChoNACHAConfiguration config = new ChoNACHAConfiguration(); config.DestinationBankRoutingNumber = "123456789"; config.OriginatingCompanyId = "123456789"; config.DestinationBankName = "PNC Bank"; config.OriginatingCompanyName = "Microsoft Inc."; config.ReferenceCode = "Internal Use Only."; config.BlockingFactor = 10;
+                using (var nachaWriter = new ChoNACHAWriter(@"NACHA_TEST.txt", config))
+                {
+                    nachaWriter.Configuration.ErrorMode = ChoETL.ChoErrorMode.IgnoreAndContinue;
 
+                    using (var bw1 = nachaWriter.CreateBatch(200))
+                    {
+                        using (var entry1 = bw1.CreateDebitEntryDetail(20, "123456789", "1313131313", 22.505M, "ID Number", "ID Name", "Desc Data"))
+                        {
+                            entry1.CreateAddendaRecord("Monthly bill");
+                        }
+                        using (var entry2 = bw1.CreateCreditEntryDetail(20, "123456789", "1313131313", 22.505M, "ID Number", "ID Name", "Desc Data"))
+                        {
+
+                        }
+                    }
+                    using (var bw2 = nachaWriter.CreateBatch(200))
+                    {
+                    }
+                }
+            }
+
+        }
+        
         static void WriteACHFile()
         {
             ChoETLFrxBootstrap.TraceLevel = System.Diagnostics.TraceLevel.Verbose;
